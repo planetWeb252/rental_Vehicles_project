52bfacee39dbb131ac5b4f29c786a5bd0a0c506e
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-05-12 14:50:04 +0200

    customer login init and employee created, insert vehicles

--- src/main/java/com/rental/vehicles/controller/routePublic/VehicleController/VehicleController.java
+++ src/main/java/com/rental/vehicles/controller/routePublic/VehicleController/VehicleController.java
@@ -1,10 +1,14 @@
 package com.rental.vehicles.controller.routePublic.VehicleController;
 
+import com.rental.vehicles.models.modelsAbstrac.Vehicle;
 import com.rental.vehicles.services.vehicle.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/public/vehicle")
@@ -17,8 +21,8 @@ public class VehicleController {
     }
 
     @GetMapping("/allVehicles")
-    public String getAllVehicles() {
-        vehicleService.allVehicles();
+    public ResponseEntity<List<Vehicle>> getAllVehicles() {
+         return vehicleService.allVehicles();
     }
 
 
